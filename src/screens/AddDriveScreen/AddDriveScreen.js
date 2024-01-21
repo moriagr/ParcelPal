939d3a860bffc7323e89ad76e0c1d19fab33a4b4
shirelzz <99799701+shirelzz@users.noreply.github.com>
2024-01-21 16:29:12 +0200

    add saving to db implementation

--- src/screens/AddDriveScreen/AddDriveScreen.js
+++ src/screens/AddDriveScreen/AddDriveScreen.js
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button , StyleSheet } from 'react-native';
-import database from '@react-native-firebase/database';
+import { View, TextInput, Button , StyleSheet, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import firebase from './../../firebase/config'
 
 const AddDriveScreen = () => {
   const [source, setSource] = useState('');
   const [destination, setDestination] = useState('');
   const [date, setDate] = useState('');
+  const navigation = useNavigation();
 
-  const saveDrive2DB = () => {
-    database()
-      .ref('/drives')
-      .push({
-        source,
-        destination,
-        date,
-      })
-      .then(() => {
-        console.log('Drive saved to Firebase!');
-      })
-      .catch((error) => {
-        console.error('Error saving drive to Firebase: ', error);
-      });
+  const saveDrive2DB = async () => {
+    try {
+      const currentUser = firebase.auth().currentUser;
+
+      if (currentUser) {
+        const userId = currentUser.uid;
+
+        const drivesRef = firebase.firestore().collection(`users/${userId}/drives`);
+        const newDriveRef = drivesRef.doc();
+
+        await newDriveRef.set({
+          source,
+          destination,
+          date,
+        });
+
+        console.log('Drive saved to Firestore!');
+        navigation.goBack();
+
+      } else {
+        console.error('No current user found');
+      }
+    } catch (error) {
+      console.error('Error saving drive to Firestore:', error);
+    }
   };
 
   return (
@@ -39,11 +52,15 @@ const AddDriveScreen = () => {
       />
       <TextInput
         style={styles.input}
-        placeholder="Date"
+        placeholder="Date and time"
         value={date}
         onChangeText={setDate}
       />
-      <Button title="Post Drive" onPress={saveDrive2DB} />
+      <Button
+        title="Post"
+        onPress={saveDrive2DB}
+        style={styles.postButton}
+      />
     </View>
   );
 };
@@ -62,6 +79,22 @@ const styles = StyleSheet.create({
       marginBottom: 12,
       padding: 8,
       width: '100%',
+    },
+    postButton: {
+        ...Platform.select({
+          ios: {
+            padding: 15,
+            borderRadius: 8,
+            backgroundColor: 'black',
+          },
+          android: {
+            backgroundColor: '#3498db',
+            padding: 15,
+            borderRadius: 8,
+            elevation: 3,
+            backgroundColor: 'black',
+          },
+        }),
     },
   });
 
