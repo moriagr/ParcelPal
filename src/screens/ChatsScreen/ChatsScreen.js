875525909d9a41f2509e202dfede72133b034cae
moriagro <moriagrohar@carmel6000.amitnet.org>
2024-02-05 12:45:05 +0200

    Fix problems at chat screen

--- src/screens/ChatsScreen/ChatsScreen.js
+++ src/screens/ChatsScreen/ChatsScreen.js
@@ -18,29 +18,29 @@ const ChatsScreen = ({ navigation }) => {
 
                 try {
                     const chatsPromises = chatsSnapshot.docs.map(async (driverDoc) => {
-                        console.log('✌️driverDoc --->', await driverDoc);
                         try {
 
                             const splitUser = driverDoc.id.split("_");
-                            let getIndexUser = splitUser.indexOf(user.id);
-                            getIndexUser = getIndexUser === 1 ? 0 : 1;
-
-                            if (getIndexUser !== -1) {
-                                const drivesUserRef = await firebase.firestore().collection('users')
-                                    .where('id', '==', splitUser[getIndexUser])
-                                    .get();
-
-                                const userChatData = drivesUserRef.docs.map((userDoc) => userDoc.data())[0];
-
-                                return {
-                                    id: userChatData.id,
-                                    email: userChatData.email,
-                                    fullName: userChatData.fullName,
-                                    phone: userChatData.phone,
-                                    profilePicture: userChatData.profilePicture,
-                                    role: userChatData.role
-                                };
+                            if (splitUser[1] !== user.id) {
+                                return null;
                             }
+
+                            const drivesUserRef = await firebase.firestore().collection('users')
+                                .where('id', '==', splitUser[0])
+                                .where('role', '==', "Driver")
+                                .get();
+
+                            const userChatData = drivesUserRef.docs.map((userDoc) => userDoc.data())[0];
+
+                            return {
+                                id: userChatData.id,
+                                email: userChatData.email,
+                                fullName: userChatData.fullName,
+                                phone: userChatData.phone,
+                                profilePicture: userChatData.profilePicture,
+                                role: userChatData.role
+                            };
+
 
                         } catch (error) {
                             console.log('✌️error --->', error);
@@ -76,7 +76,7 @@ const ChatsScreen = ({ navigation }) => {
 
                     const drivesPromises = await unique.map(async (currUserId) => {
                         const userClient = await firebase.firestore().collection('users').where("id", "==", currUserId).get();
-                        console.log('✌️userClient --->', userClient.docs);
+
                         const drivesData = userClient.docs.map((driveDoc) => driveDoc.data())[0];
                         return {
                             id: drivesData.id,
